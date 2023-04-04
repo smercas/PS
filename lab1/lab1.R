0d07b1fe8e633c46f8b22d779ee6cb9b6b169057
smercas <smercas@gmail.com>
2023-04-04 17:30:49 +0300

    laborator1

--- lab1/lab1.R
+++ lab1/lab1.R
@@ -2,15 +2,17 @@ ppPoisson = function (halflife, n) {
   x = 1:n;
   y = dpois(x, halflife);
   barplot(y);
-  x
+  return (y);
 }
 ppGeometric = function (p, n) {
   x = 1:n
   y = dgeom(x, p);
   barplot(y);
-  y
+  return (y);
 }
 x = scan("my_file.txt");
 
 ppGeometric(1/36, 800);
-ppPoisson(0.1, 800);
+ppPoisson(0.1, 800)
+
+
